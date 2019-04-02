026c646cd29cb4a39775af984fb8f28fcbf42565
Pazpok <mathebossdu22@hotmail.fr>
2019-04-02 22:16:17 +0200

    templating

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,9 @@
 import bootstrap from 'bootstrap';
 import $ from 'jquery'
-import select2 from 'select2';
+import select2 from 'select2/dist/js/select2.min'
 import "../css/app.scss";
 
 
 $(document).ready(function() {
     $('.genre-select').select2();
-    $('#user_isArtist').change(function () {
-        if (!this.checked)
-            $('#user div').slice(8,17).fadeOut('fast');
-        else
-            $('#user div').slice(8,17).fadeIn('fast');
-    })
 });
